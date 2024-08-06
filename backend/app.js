8cdd0c4028aad8efef94300eac010b5157e02c44
utkarsh2314 <utkarshukla2003@gmail.com>
2024-08-06 10:35:20 +0530

    added automatic mail service by nodecron

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import userRouter from "./routes/userRouter.js";
 import fileUpload from "express-fileupload";
 import jobRouter from "./routes/jobRouter.js";
 import applicationRouter from "./routes/applicationRouter.js";
+import { newsLetterCron } from "./automation/newsLetterCron.js";
 
 const app = express();
 config({ path: "./config/config.env" });
@@ -35,6 +36,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 
+newsLetterCron();
 connection();
 app.use(errorMiddleware);
 
