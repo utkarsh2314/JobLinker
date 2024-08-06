09d0dba66386929d66368b69f4ae4c63bb815607
utkarsh2314 <utkarshukla2003@gmail.com>
2024-08-06 08:27:35 +0530

    added login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,3 +77,24 @@ export const register = catchAsyncErrors(async (req, res, next) => {
     next(error);
   }
 });
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { role, email, password } = req.body;
+  if (!role || !email || !password) {
+    return next(
+      new ErrorHandler("Email, password and role are required.", 400)
+    );
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password.", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password.", 400));
+  }
+  if (user.role !== role) {
+    return next(new ErrorHandler("Invalid user role.", 400));
+  }
+  sendToken(user, 200, res, "User logged in successfully.");
+});
