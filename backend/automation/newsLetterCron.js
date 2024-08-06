6c0d0f964e59703d7186cde5503e1a1e77c3e7a8
utkarsh2314 <utkarshukla2003@gmail.com>
2024-08-06 10:45:12 +0530

    completed backend

--- backend/automation/newsLetterCron.js
+++ backend/automation/newsLetterCron.js
@@ -18,7 +18,7 @@ export const newsLetterCron = () => {
         });
         for (const user of filteredUsers) {
           const subject = `Hot Job Alert: ${job.title} in ${job.jobNiche} Available Now`;
-          const message = `Hi ${user.name},\n\nGreat news! A new job that fits your niche has just been posted. The position is for a ${job.title} with ${job.companyName}, and they are looking to hire immediately.\n\nJob Details:\n- **Position:** ${job.title}\n- **Company:** ${job.companyName}\n- **Location:** ${job.location}\n- **Salary:** ${job.salary}\n\nDon’t wait too long! Job openings like these are filled quickly. \n\nWe’re here to support you in your job search. Best of luck!\n\nBest Regards,\nNicheNest Team`;
+          const message = `Hi ${user.name},\n\nGreat news! A new job that fits your niche has just been posted. The position is for a ${job.title} with ${job.companyName}, and they are looking to hire immediately.\n\nJob Details:\n- **Position:** ${job.title}\n- **Company:** ${job.companyName}\n- **Location:** ${job.location}\n- **Salary:** ${job.salary}\n\nDon’t wait too long! Job openings like these are filled quickly. \n\nWe’re here to support you in your job search. ALL the best!\n\nBest Regards,\nJobLinker Team`;
           sendEmail({
             email: user.email,
             subject,
