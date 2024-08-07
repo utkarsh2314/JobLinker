6654b634bc8359428e688be7101d05baffde6012
utkarsh2314 <utkarshukla2003@gmail.com>
2024-08-08 00:38:43 +0530

    initial frontend setup complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Jobs from "./pages/Jobs";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import PostApplication from "./pages/PostApplication";
+import Register from "./pages/Register";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/post/application/:jobId"
+            element={<PostApplication />}
+          />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <ToastContainer position="top-right" theme="dark" />
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/PostApplication.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostApplication = () => {
+  return <div></div>;
+};
+
+export default PostApplication;
