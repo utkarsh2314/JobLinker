1d30a0626b8dbb4a9b1cda2e2c47ca9c725959d1
utkarsh2314 <utkarshukla2003@gmail.com>
2024-08-17 17:18:05 +0530

    added spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { ClipLoader } from "react-spinners";
 
 const Spinner = () => {
-  return <div>Spinner</div>;
+  return (
+    <>
+      <section
+        style={{
+          minHeight: "525px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ClipLoader size={150} />
+      </section>
+    </>
+  );
 };
 
 export default Spinner;
